f240cae47ab9dcd52b62cc72ecdec8c8550f08ac
Bryce <bryceschulle@yahoo.com>
2024-11-07 16:21:16 -0800

    initial commit for index.html and index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+//Send user message
+alert ("You have received this message because you have been chosen to open an important vault.");
+
+// Create vault code
+const firstValue = 40 / 4 ;
+const secondValue = 50 - 10 ;
+const thirdValue = 13 * 3 ;
+
+//send user vault code
+alert ("Here is the secret combination: 10 - 40 - 39");
+console.log (`${firstValue} - ${secondValue} - ${thirdValue}`);
